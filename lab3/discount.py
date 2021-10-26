d1500648d452772c4c54747576809f68d8516584
ifeniak <ivan.feniak@gmail.com>
2021-10-26 15:31:51 +0300

    added code to lab3

---
+++ lab3/discount.py
@@ -0,0 +1,18 @@
+import collections
+
+
+def best_purchase(prices: list, discount):
+    prices.sort()
+    prices = collections.deque(prices)
+    min_sum = 0
+    for _ in range(len(prices) // 3):
+        min_sum += max(0, prices.popleft())
+        min_sum += prices.popleft()
+        min_sum += prices.pop() * (100 - discount) / 100
+    if prices is not None:
+        min_sum += sum(prices)
+    return min_sum
+
+
+prices = [10000, 1, 0]
+print(best_purchase(prices, 33))
